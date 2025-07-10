4c79277ece1a08d02522246c3e865969acaf442e
gauciv <vincentaugusto16@gmail.com>
2025-07-11 00:03:03 +0800

    fix: resolve invalid favicon path error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,6 +81,10 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-icon.png',
+  },
 }
 
 export default function RootLayout({
